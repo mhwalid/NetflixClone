51a42ab8ba117387442b8b7eff187ff99fd9c1df
Basile2121 <basile.regnault@gmail.com>
2022-02-02 16:33:14 +0100

    [Footer] Structure du footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer>
+    <p>Copy 2022 Clone Netflix, Inc.</p>
+    <p>Basile Regnault</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
+import Footer from '@/components/Footer';
 
 export default {
   name: 'Home',
   components: {
+    Footer,
     HelloWorld
   }
 }
