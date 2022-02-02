81cf622da7644697f4b5d3e34996971d61098943
Fabien <fabienponcet.69@gmail.com>
2022-02-02 16:00:42 +0100

    [Navbar] Création de la structure de la navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+      <div class="netflixLogo">
+        <a id="logo" href="#home"><img src="https://github.com/carlosavilae/Netflix-Clone/blob/master/img/logo.PNG?raw=true" alt="Logo Image"></a>
+      </div>      
+      <nav class="main-nav">                
+        <router-link to="/">Accueil</router-link>
+        <a href="#movies">Films</a>
+        <a href="#originals">Séries</a>
+        <a href="#">Récemment ajouté</a>        
+      </nav>   
+    </header>
+</template>
+
+<script>
+export default {
+    name : "Navbar",
+
+}
+</script>
+
+<style>
+
+a {
+    text-decoration: none;
+    color: white;
+}
+
+nav {
+    background-color:black;
+}
+</style>
