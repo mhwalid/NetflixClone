804b6f203f8ec412cba3fd3ddddbf8374b584525
Basile2121 <basile.regnault@gmail.com>
2022-02-02 16:39:09 +0100

    [Footer] Style du footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,5 +14,16 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
+footer {
+  padding: 20px;
+  text-align: center;
+  color: white;
+  margin: 10px;
+  background-color: black;
+}
+
+p {
+  font-weight: bold;
+}
 
 </style>
